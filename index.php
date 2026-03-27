91e06505a5aa51e326c1c577f983cd9d9f41b1d8
Douglas Castro <castroctt@gmail.com>
2026-03-27 12:50:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto Biblioteca</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+    include("menu.php");
+    ?>
+    <div class="content">
+        <div class="content">
+        <h1>Biblioteca Digital Municipal</h1>
+        <p>A Biblioteca Digital Municipal tem como missão promover o acesso ao conhecimento, à cultura e à educação por
+            meio de um acervo digital acessível a toda a comunidade.</p>
+        <br>
+        <p>Nosso sitema permite consultar obras literárias, acessar livros digitais e incentivar a leitura através da
+            tecnologia
+            Este ambiente foi desenvolvido para facilitar o gereniamento do acervo bibliográfico e aproximar leitores do
+            universo literário.
+        </p>
+    </div>
+    </div>
+    <img src="https://www.cultuga.com.br/wp-content/uploads/2017/02/real-gabinete-portugues-rio-de-janeiro-Edu-Mendes-biblioteca-capa.jpg"
+        alt="Biblioteca do Rio de Janeiro" style="width: 100%;">
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,6 @@
+<div class="sidebar">
+    <h2>Biblioteca</h2>
+    <a href="index.php">Início</a>
+    <a href="index.php">Acervo</a>
+    <a href="index.php">Cadastrar Livro</a>
+</div>
